f6508981f13a7f01d006dd109d4c3c66fbb2c174
User <User@192.168.0.102>
2020-04-21 16:34:57 -0300

    Cheiro de pneu queimado Carburador furado o x9 foi torrado

--- src/startenglish/StartEnglish.java
+++ src/startenglish/StartEnglish.java
@@ -24,6 +24,8 @@ public class StartEnglish extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.setResizable(false);
         stage.show();
     }
 

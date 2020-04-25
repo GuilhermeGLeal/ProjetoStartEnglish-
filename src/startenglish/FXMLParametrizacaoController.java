d5755810e08df20c1caa09ce3a643c7f45bd7ae1
guilh <guilh@DESKTOP-U3A4A70>
2020-04-25 10:52:02 -0300

    Classe de mascaras e afins

--- src/startenglish/FXMLParametrizacaoController.java
+++ src/startenglish/FXMLParametrizacaoController.java
@@ -1,11 +1,49 @@
 package startenglish;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 
 public class FXMLParametrizacaoController implements Initializable {
+
+    @FXML
+    private JFXButton btNovo;
+    @FXML
+    private JFXButton btAlterar;
+    @FXML
+    private JFXButton btApagar;
+    @FXML
+    private JFXButton btConfirmar;
+    @FXML
+    private JFXButton btCancelar;
+    @FXML
+    private JFXTextField txNome;
+    @FXML
+    private JFXButton btEscolheArquivo;
+    @FXML
+    private JFXButton btLimparFoto;
+    @FXML
+    private JFXTextField txTelefone;
+    @FXML
+    private JFXTextField txRazao;
+    @FXML
+    private JFXTextField txEmail;
+    @FXML
+    private JFXTextField txIDendereco;
+    @FXML
+    private JFXTextField txRua;
+    @FXML
+    private JFXTextField txCEP;
+    @FXML
+    private JFXTextField txBairro;
+    @FXML
+    private JFXTextField txNumero;
+    @FXML
+    private JFXTextField txCidade;
 
    
     @Override

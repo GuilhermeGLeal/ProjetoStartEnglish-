35a1013253abeff7e1372793730f6872729df6a5
guilh <guilh@DESKTOP-U3A4A70>
2020-04-25 12:26:37 -0300

    AJSHASHAHS

--- src/startenglish/db/DAL/DALEndereco.java
+++ src/startenglish/db/DAL/DALEndereco.java
@@ -1,29 +1,84 @@
 package startenglish.db.DAL;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import startenglish.db.Entidades.Endereco;
+import startenglish.db.util.Banco;
 
 
 public class DALEndereco {
  
      public boolean gravar(Endereco e){
         
+        String sql = "insert into Endereco(Rua,CEP,Bairro,Numero,Cidade) values('#1','#2','#3','#4','#5')";
+        sql = sql.replaceAll("#1", e.getRua());
+        sql = sql.replaceAll("#2", e.getCEP());
+        sql = sql.replaceAll("#3", e.getBairro());
+        sql = sql.replaceAll("#4", ""+e.getNumero());
+        sql = sql.replaceAll("#5", e.getCidade());
+        
+        return Banco.getCon().manipular(sql);
+        
     }
     
-    public boolean alterar(Endereco p){
+    public boolean alterar(Endereco e){
         
+       String sql = "update Endereco set Rua = '#1', CEP = '#2', Bairro='#3', Numero= #4, Cidade='#5' where EnderecoID = "+e.getEnderecoID();
+        sql = sql.replaceAll("#1", e.getRua());
+        sql = sql.replaceAll("#2", e.getCEP());
+        sql = sql.replaceAll("#3", e.getBairro());
+        sql = sql.replaceAll("#4", ""+e.getNumero());
+        sql = sql.replaceAll("#5", e.getCidade());
         
+        return Banco.getCon().manipular(sql);
     }
     
-    public boolean apagar(Endereco p){
+    public boolean apagar(Endereco e){
         
+        return Banco.getCon().manipular("delete from Endereco where EnderecoID="+e.getEnderecoID());
     }
     
     public Endereco get(int cod){
+     
+        Endereco ender = null;
+        ResultSet rs = Banco.getCon().consultar("select * from Endereco where EnderecoID="+cod);
         
+        try{
+            
+            if(rs.next())
+            {
+                ender = new Endereco(rs.getInt("EnderecoID"), rs.getString("Rua"), rs.getString("CEP"), rs.getString("Bairro"), rs.getInt("Numero"), rs.getString("Cidade"));
+                 
+             }
+        }
+        catch(SQLException e ){System.out.println(e.getMessage());}
+        
+        return ender;
     }
     
     public List<Endereco> get(String filtro){
         
+       String sql="select *from Endereco";
+       
+        if(!filtro.isEmpty())
+            sql+=" where "+filtro;
+        
+        List <Endereco> aux = new ArrayList();
+        ResultSet rs = Banco.getCon().consultar(sql);
+        try 
+        {
+            while(rs.next())
+            {
+                aux.add(new Endereco(rs.getInt("EnderecoID"), rs.getString("Rua"), rs.getString("CEP"), rs.getString("Bairro"), rs.getInt("Numero"), rs.getString("Cidade")));
+            }
+        } 
+        catch (SQLException ex) 
+        {
+            System.out.println(ex.getMessage());
+        }
+        
+        return aux;
     }
 }
--- src/startenglish/db/DAL/DALParametrizacao.java
+++ src/startenglish/db/DAL/DALParametrizacao.java
@@ -2,38 +2,73 @@ package startenglish.db.DAL;
 
 import java.io.File;
 import java.io.InputStream;
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import startenglish.db.Entidades.Parametrizacao;
+import startenglish.db.util.Banco;
 
 
 public class DALParametrizacao {
     
     public boolean gravar(Parametrizacao p){
         
+        String sql = "insert into Parametrizacao(NomeEmpresa,Telefone,RazaoSocial,E-mail,EnderecoID) values('#1','#2','#3','#4',#5)";
+        sql = sql.replaceAll("#1",p.getNome());
+        sql = sql.replaceAll("#2",p.getTelefone());
+        sql = sql.replaceAll("#3",p.getRazaoSocial());
+        sql = sql.replaceAll("#4",p.getEmail());
+        sql = sql.replaceAll("#5",""+p.getEndereco().getEnderecoID());
+             
+        return Banco.getCon().manipular(sql);
+        
     }
     
     public boolean alterar(Parametrizacao p){
         
+        String sql = "update Parametrizacao set NomeEmpresa = '#1', Telefone = '#2', RazaoSocial='#3', E-mail= '#4', EnderecoID=#5 where NomeEmpresa = "+p.getNome();
+        sql = sql.replaceAll("#1",p.getNome());
+        sql = sql.replaceAll("#2",p.getTelefone());
+        sql = sql.replaceAll("#3",p.getRazaoSocial());
+        sql = sql.replaceAll("#4",p.getEmail());
+        sql = sql.replaceAll("#5",""+p.getEndereco().getEnderecoID());
         
+        return Banco.getCon().manipular(sql);
     }
     
     public boolean apagar(Parametrizacao p){
         
+        return Banco.getCon().manipular("delete from Parametrizacao where NomeEmpresa="+p.getNome());
     }
     
-    public Parametrizacao get(int cod){
+    public Parametrizacao get(String nome){
         
-    }
-    
-    public List<Parametrizacao> get(String filtro){
+        Parametrizacao paramet = null;
+        ResultSet rs = Banco.getCon().consultar("select NomeEmpresa,Telefone,RazaoSociala,E-mail,EnderecoID from Parametrizacao where NomeEmpresa="+nome);
         
+        DALEndereco dal = new DALEndereco();
+        
+        try{
+            
+            if(rs.next())
+            {
+                paramet = new Parametrizacao(rs.getString("NomeEmpresa"),rs.getString("NomeEmpresa"),rs.getString("NomeEmpresa"),rs.getString("NomeEmpresa"),
+                        dal.get(rs.getInt("EnderecoID")));
+                 
+             }
+        }
+        catch(SQLException e ){System.out.println(e.getMessage());}
+        
+        return paramet;
     }
+       
     
     public boolean gravarFoto(int cod,File arq){
         
+        return false;
     }
     
     public InputStream getFoto(int cod){
          
+        return null;
      }
 }

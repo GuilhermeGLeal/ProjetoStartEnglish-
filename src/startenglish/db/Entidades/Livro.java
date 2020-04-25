7e683a167ad8674097e095f7ddea6278aefe7183
guilh <guilh@DESKTOP-U3A4A70>
2020-04-25 13:33:17 -0300

    AJSHASHAHS

--- src/startenglish/db/Entidades/Livro.java
+++ src/startenglish/db/Entidades/Livro.java
@@ -3,5 +3,48 @@ package startenglish.db.Entidades;
 
 public class Livro 
 {
+    private int LivroID;
+    private String Nome;
+    private double valor;
+
+    public Livro(int LivroID, String Nome, double valor) {
+        this.LivroID = LivroID;
+        this.Nome = Nome;
+        this.valor = valor;
+    }
+
+    public Livro(String Nome, double valor) {
+        this.Nome = Nome;
+        this.valor = valor;
+    }
+
+    public Livro() {
+    }
+
+    
+    public int getLivroID() {
+        return LivroID;
+    }
+
+    public void setLivroID(int LivroID) {
+        this.LivroID = LivroID;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
     
 }

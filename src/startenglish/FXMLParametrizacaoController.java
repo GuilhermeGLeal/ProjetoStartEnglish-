28391c0d85b9e8ab831e0f5f265da592dc6ed483
guilh <guilh@DESKTOP-U3A4A70>
2020-04-25 15:52:20 -0300

    JAJAJAJSAJSAHHSAHS

--- src/startenglish/FXMLParametrizacaoController.java
+++ src/startenglish/FXMLParametrizacaoController.java
@@ -3,10 +3,25 @@ package startenglish;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputControl;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import startenglish.db.DAL.DALParametrizacao;
+import startenglish.db.Entidades.Parametrizacao;
+import startenglish.util.MaskFieldUtil;
 
 
 public class FXMLParametrizacaoController implements Initializable {
@@ -45,15 +60,82 @@ public class FXMLParametrizacaoController implements Initializable {
     private JFXTextField txNumero;
     @FXML
     private JFXTextField txCidade;
+    @FXML
+    private TableView<Parametrizacao> tabela;
+    @FXML
+    private TableColumn<Parametrizacao, String> coluaNome;
+    @FXML
+    private TableColumn<Parametrizacao, String> colunaRazaoSocial;
+    @FXML
+    private TableColumn<Parametrizacao, String> ColunaTelefone;
+    @FXML
+    private AnchorPane pndados;
+    @FXML
+    private ImageView img;
 
    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        coluaNome.setCellValueFactory(new PropertyValueFactory("nome"));
+        colunaRazaoSocial.setCellValueFactory(new PropertyValueFactory("RazaoSocial"));
+        ColunaTelefone.setCellValueFactory(new PropertyValueFactory("telefone"));
+        
+        MaskFieldUtil.maxField(txNome, 30);
+        MaskFieldUtil.foneField(txTelefone);
+        MaskFieldUtil.maxField(txRazao, 20);
+        MaskFieldUtil.maxField(txEmail, 20);
+        MaskFieldUtil.maxField(txRua, 20);
+        MaskFieldUtil.cepField(txCEP);
+        MaskFieldUtil.maxField(txBairro, 30);
+        MaskFieldUtil.numericField(txNumero);
+        MaskFieldUtil.maxField(txCidade, 20);
+        
+        estadoOriginal();
     }    
 
+    private void carregarTabela(String filtro){
+     
+        DALParametrizacao dalpar = new DALParametrizacao();
+        Parametrizacao para = new Parametrizacao();
+        para = dalpar.get();
+        tabela.setItems((ObservableList<Parametrizacao>) para);
+    }
+    
+    private void estadoOriginal(){
+               
+        pndados.setDisable(true);
+        btConfirmar.setDisable(true);
+        btCancelar.setDisable(false);
+        btApagar.setDisable(true);
+        btAlterar.setDisable(true);
+        btNovo.setDisable(false);
+        txNumero.setDisable(true);
+        
+        ObservableList <Node> componentes=pndados.getChildren(); 
+        for(Node n : componentes)
+        {
+            if (n instanceof TextInputControl)  
+                ((TextInputControl)n).setText("");
+            if(n instanceof ComboBox)
+                ((ComboBox)n).getItems().clear();
+        }
+    }
+    
+    private void estadoedicao(){
+        
+        pndados.setDisable(false);
+        btConfirmar.setDisable(false);
+        btApagar.setDisable(true);
+        btAlterar.setDisable(true);
+        txNome.requestFocus();
+         
+    }
+    
     @FXML
     private void evtNovo(ActionEvent event) {
+        
+          estadoedicao();    
     }
 
     @FXML
@@ -78,6 +160,18 @@ public class FXMLParametrizacaoController implements Initializable {
 
     @FXML
     private void evtLimpar(ActionEvent event) {
+        
+        img.setImage(null);
+    }
+
+    @FXML
+    private void clickTabela(MouseEvent event) {
+        
+          if(tabela.getSelectionModel().getSelectedIndex()>=0){
+            
+            btAlterar.setDisable(false);
+            btApagar.setDisable(false);
+        }
     }
     
 }

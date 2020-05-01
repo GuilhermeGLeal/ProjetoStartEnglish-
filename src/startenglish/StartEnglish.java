de12f3c789694cf6506fb7e795ec45cad645538f
guilh <guilh@DESKTOP-U3A4A70>
2020-05-01 14:29:40 -0300

    Cheiro de pemneu queimado

--- src/startenglish/StartEnglish.java
+++ src/startenglish/StartEnglish.java
@@ -24,14 +24,14 @@ public class StartEnglish extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLParametrizacao.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
         
         Scene scene = new Scene(root);
         scene.setFill(Color.TRANSPARENT);
         
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
-        stage.setResizable(false);
+        stage.setMaximized(true);
         stage.show();
         
     }

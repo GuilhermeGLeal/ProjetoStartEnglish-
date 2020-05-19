8492b9cb5ed32f5e33efd0fa9ec03c5b4d3e34fb
guilh <guilh@DESKTOP-U3A4A70>
2020-05-18 21:55:04 -0300

    Terminando a tela de agendar

--- src/startenglish/FXMLAgendarProvaController.java
+++ src/startenglish/FXMLAgendarProvaController.java
@@ -4,9 +4,15 @@ package startenglish;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,6 +20,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
+import startenglish.db.DAL.DALAluno;
+import startenglish.db.DAL.DALProfessor;
 import startenglish.db.Entidades.Agenda;
 import startenglish.db.Entidades.Aluno;
 import startenglish.db.Entidades.Professor;
@@ -61,28 +69,112 @@ public class FXMLAgendarProvaController implements Initializable {
     private JFXDatePicker dtDataFIMf;
     @FXML
     private JFXDatePicker dtDataInif;
-    @FXML
-    private JFXComboBox<Aluno> cbAlunoF;
-    @FXML
+   @FXML
     private JFXButton btApagar;
     @FXML
     private JFXButton btAlterar;
-    @FXML
-    private JFXComboBox<Professor> cbProfessorF;
     @FXML
     private JFXButton btPesquisar;
     @FXML
     private JFXButton btLimpar;
     @FXML
     private TableView<Agenda> tabela;
+    @FXML
+    private JFXTextField txAluno;
+    @FXML
+    private JFXTextField txProf;
 
    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
-      
-    }    
+    
+        seta_comboboxBanco();
+        seta_combobox();
+        estadoOriginal();
+        seta_pesquisa();
+    }
 
+    public void seta_combobox(){
+    
+        List<String> Local = new ArrayList();
+        List<String> Horario = new ArrayList();
+        List<String> Status = new ArrayList();
+        List<String> Filtro = new ArrayList();
+
+        Status.add("Esperando");
+        Status.add("Faltou");
+        Status.add("Realizou");
+        Filtro.add("Aluno");
+        Filtro.add("Data");
+        Filtro.add("Professor");
+        Local.add("Sede");
+        Horario.add("13:30 as 15:30");
+        Horario.add("08:30 as 10:30");
+
+        ObservableList<String> modelostatus = FXCollections.observableArrayList(Status);
+        ObservableList<String> modelolocal = FXCollections.observableArrayList(Local);
+        ObservableList<String> modelofiltro = FXCollections.observableArrayList(Filtro);
+        ObservableList<String> modelohorario = FXCollections.observableArrayList(Horario);
+  
+        cbStatus.setItems(modelostatus);
+        cbFiltro.setItems(modelofiltro);
+        cbLocal.setItems(modelolocal);
+        cbHorario.setItems(modelohorario);
+        
+        cbStatus.setValue("Realizou");
+        cbFiltro.setValue("Aluno");
+        cbHorario.setValue("08:30 as 10:30");
+        cbLocal.setValue("Sede");
+    }
+
+    public void carregaTabela(String filtro){
+        
+    }
+    
+    private void estado_edicao(){
+        
+    }
+    
+    private void seta_pesquisa(){
+        
+        txProf.clear();
+        txProf.setDisable(true);
+        txAluno.clear();
+        txAluno.setDisable(false);
+        cbFiltro.setValue("Aluno");
+        dtDataInif.setDisable(true);
+        dtDataFIMf.setDisable(true);
+        dtDataInif.setValue(LocalDate.now());
+        dtDataFIMf.setValue(LocalDate.now().plusDays(30));
+        
+    }
+    
+    private void estadoOriginal(){
+        
+        btAlterar.setDisable(true);
+        btApagar.setDisable(true);
+        btSalvarOp.setDisable(true);
+        dtDataAgend.requestFocus();
+    }
+    
+    public void seta_comboboxBanco(){
+     
+        DALProfessor dalp = new DALProfessor();
+        DALAluno dala = new DALAluno();
+        List<Professor> profs = new ArrayList();
+        List<Aluno> alunos =  new ArrayList();
+        
+        profs = dalp.get("");
+        alunos = dala.get("");
+        
+        ObservableList<Professor> modelopf =  FXCollections.observableArrayList(profs);
+        ObservableList<Aluno> modeloaluno =  FXCollections.observableArrayList(alunos);
+        
+        cbProfessor.setItems(modelopf);
+        cbAluno.setItems(modeloaluno);
+        
+    }
+    
     @FXML
     private void evtNovo(ActionEvent event) {
     }
@@ -109,6 +201,28 @@ public class FXMLAgendarProvaController implements Initializable {
 
     @FXML
     private void evtFiltroComb(ActionEvent event) {
+        
+        String filtro = cbFiltro.getSelectionModel().getSelectedItem();
+        
+        if(filtro.contains("Data")){
+            
+            dtDataInif.setDisable(false);
+            dtDataFIMf.setDisable(false);
+            txAluno.setDisable(true);
+            txProf.setDisable(true);
+        }
+        else{
+                            
+            dtDataInif.setDisable(true);
+            dtDataFIMf.setDisable(true);
+            
+            if(filtro.contains("Aluno")){
+                
+                txAluno.setDisable(false);
+                txProf.setDisable(true);
+            }
+              
+        }
     }
 
     @FXML
@@ -125,6 +239,18 @@ public class FXMLAgendarProvaController implements Initializable {
 
     @FXML
     private void evtLimpar(ActionEvent event) {
+        
+        seta_pesquisa();
+    }
+
+    @FXML
+    private void evTabela(MouseEvent event) {
+        
+          if(tabela.getSelectionModel().getSelectedIndex()>=0)
+        {
+           btApagar.setDisable(false);
+           btAlterar.setDisable(false);
+        }
     }
     
 }

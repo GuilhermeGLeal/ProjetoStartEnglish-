3ef1dbe3586645f2bd6a60b6aaa3d813f0be9e89
azeve <azeve@DESKTOP-9S0N3CT>
2020-05-26 17:51:47 -0300

    Commit Faltas

--- src/startenglish/StartEnglish.java
+++ src/startenglish/StartEnglish.java
@@ -24,7 +24,7 @@ public class StartEnglish extends Application
 {   
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/FXMLFaltas.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/FXMLPrincipal.fxml"));
         
         Scene scene = new Scene(root);
       
